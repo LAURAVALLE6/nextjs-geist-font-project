a992a8f1fd1a4ac89fbc535a918297043385e020
LAURAVALLE6 <laura.valle07@gmail.com>
2025-06-12 17:57:42 -0300

    Add .next/static/chunks/src_app_dashboard_layout_tsx_36d00ef6._.js 1749761861667

---
+++ .next/static/chunks/src_app_dashboard_layout_tsx_36d00ef6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_13c975c1._.js",
+  "static/chunks/src_daa01866._.js"
+],
+    source: "dynamic"
+});
